cf30cea85cef3f4ad656e800997b4e22b354fc39
Ujjwal Solanki <75205571+ujjwalsolankii@users.noreply.github.com>
2023-07-02 22:45:22 +0530

    Add files via upload

---
+++ initailization.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class GFG {
+    public static void main (String args[]){
+        int z = 35;                    
+        double pi = 3.14159;   
+        char retChar = 'x';               
+        double a = Math.sqrt(25);   
+        System.out.println("Sqrt of 25 is:  "+a );
+        System.out.println(z);
+        System.out.println(pi );
+        System.out.println(retChar);
+    }
+}
